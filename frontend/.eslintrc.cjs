64ce072a43eddb7b5dce580061897d211362a292
zhangzuhao <z15134084468@163.com>
2026-05-07 21:38:07 +0800

    task-T005: Configure frontend lint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist/", "node_modules/"]
+};
